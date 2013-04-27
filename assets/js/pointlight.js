081f65b7cc1f4840051e64877c966e3c72444921
Izzet <izzetStMr@gmail.com>
2013-04-27 06:21:12 +0200

    bounding circle

--- assets/js/pointlight.js
+++ assets/js/pointlight.js
@@ -4,6 +4,7 @@ function PointLight( options ){
 	
 	this.distance = options.distance === undefined ? 100 : options.distance;
 	this.color = options.color === undefined ? "#ffffff" : options.color;
+	this.radius = options.radius === undefined ? this.distance*0.8 : options.radius;
 };
 PointLight.prototype = Object.create( Object2.prototype );
 PointLight.prototype.generateCache = function (){
@@ -19,10 +20,17 @@ PointLight.prototype.generateCache = function (){
 	ctx.fill();
 	ctx.closePath();
 };
+PointLight.prototype.drawBoundingCircle = function (ctx){
+	ctx.beginPath();
+	ctx.arc(this.position.x,this.position.y,this.radius,0,Math.PI*2);
+	ctx.fillStyle = this.color;
+	ctx.stroke();
+	ctx.closePath();
+};
 PointLight.prototype.render = function (ctx){
 	if(this.cache === undefined){
 		this.generateCache();
 	}
-	ctx.drawImage(this.cache.canvas,this.position.x,this.position.y);
-	
+	ctx.drawImage(this.cache.canvas,this.position.x-this.distance,this.position.y-this.distance);
+	this.drawBoundingCircle(ctx);
 };
